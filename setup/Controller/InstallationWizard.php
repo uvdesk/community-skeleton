438a3c855cfd608a303ec6b3734984b70424e010
akshay kumar <akshay.kumar758@webkul.com>
2018-11-12 19:13:07 +0530

    misc. updates - add wizard home page

--- setup/Controller/InstallationWizard.php
+++ setup/Controller/InstallationWizard.php
@@ -8,11 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class InstallationWizard extends Controller
 {
-    const HELPDESK_VERSION = '0.1.0-DEV';
+    const HELPDESK_VERSION = '0.2.0 DEV';
 
     public function wizard($_locale)
     {
-        return $this->render('helpdesk-installation-wizard/index.html.twig', [
+        return $this->render('installation-wizard/index.html.twig', [
             'version' => self::HELPDESK_VERSION,
         ]);
     }
