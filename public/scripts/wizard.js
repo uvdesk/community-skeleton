304ea427c5a80d521a6d0b7e69871a714475dcd6
akshay kumar <akshay.kumar758@webkul.com>
2018-11-15 20:19:48 +0530

    misc. updates

--- public/scripts/wizard.js
+++ public/scripts/wizard.js
@@ -1,65 +1,82 @@
 (function ($) {
     // Wait for all assets to load
     $(window).bind("load", function() {
-        console.log('Resources loaded');
+        var UVDeskCommunitySystemRequirementsView = Backbone.View.extend({
+            el: '#wizard-system-requirements',
+            initialize: function() {
+                console.log('initialize system req.');
+            },
+        });
+
+        var InstallationWizard = Backbone.View.extend({
+            el: '#wizard',
+            router: {},
+            reference_nodes: {
+                header: undefined,
+                content: undefined,
+            },
+            wizard_default_header_template: _.template($("#installationWizard-DefaultHeaderTemplate").html()),
+            wizard_default_content_template: _.template($("#installationWizard-DefaultContentTemplate").html()),
+            events: {
+                'click #wizardInstallationCTA': 'startInstallationProcess',
+            },
+            procedureChecklist: [
+                {
+                    path: 'check-requirements',
+                    view: UVDeskCommunitySystemRequirementsView,
+                },
+                {
+                    path: 'configure-database',
+                },
+                {
+                    path: 'configure-website',
+                },
+                {
+                    path: 'setup-admin',
+                },
+                {
+                    path: 'install',
+                },
+            ],
+            initialize: function(params) {
+                this.router = params.router;
+                this.reference_nodes.header = this.$el.find('#wizardHeader');
+                this.reference_nodes.content = this.$el.find('#wizardContent');
+
+                this.renderWizard();
+            },
+            renderWizard: function() {
+                let self = this;
+
+                this.reference_nodes.header.html(self.wizard_default_header_template());
+                this.reference_nodes.content.html(self.wizard_default_content_template());
+            },
+            startInstallationProcess: function() {
+                console.log(this.procedureChecklist);
+
+                this.router.navigate('check-requirements', {
+                    trigger: true
+                });
+            },
+        });
+    
+        Router = Backbone.Router.extend({
+            wizard: undefined,
+            routes: {
+                ':installationStep': 'iterateInstallationProcedure',
+            },
+            initialize: function() {
+                let self = this;
+
+                // Initialize installation wizard
+                this.wizard = new InstallationWizard({ router: self });
+            },
+            iterateInstallationProcedure: function(installationStep) {
+                console.log('iterate step:', installationStep);
+            },
+        });
+    
+        var router = new Router();
+        Backbone.history.start({ push_state: true });
     });
-
-    // Main execution
-    console.log('Hello World');
-
-    var InstallationWizard = Backbone.View.extend({
-        el: '#wizard-container',
-        router: {},
-        collection: {},
-        initialize: function(params) {
-            this.router = params.router;
-            // this.collection.incomingSourcesStatistics = new VisitorsIncomingSourceStatisticsView({
-            //     parentViewReference: this,
-            //     elementReference: '.knowledgebase-insights.source-overview-metrics',
-            // });
-        },
-        appendLoader: function(node_reference) {
-            // this.$el.find(node_reference).append(this.loader_template());
-        },
-        removeLoader: function(node_reference) {
-            // this.$el.find(node_reference + ' .uv-loader-view').remove();
-        },
-        updateReport: function() {
-            // this.collection.incomingSourcesStatistics.reference_model.syncReports({filters: this.getValidParameters()});
-        },
-    });
-
-    Router = Backbone.Router.extend({
-        wizard: undefined,
-        routes: {
-            '': 'loadWizard',
-            // 'start/:startDate(/end/:endDate)': 'getFilteredReport',
-        },
-        initialize: function() {
-            var self = this;
-            self.wizard = new InstallationWizard({router: self});
-        },
-        loadWizard: function() {
-            console.log('Load Wizard');
-            // this.fetch();
-        },
-        // getFilteredReport: function(startDate, endDate, agent, customer, priority, type, group, team, source) {
-        //     this.resetFilter(startDate, endDate, agent, customer, priority, type, group, team, source);
-        //     this.fetch();
-        // },
-        // resetFilter: function(startDate, endDate, agent, customer, priority, type, group, team, source) {
-        //     // console.log('Reset insight filters');
-        //     $('#insightsFilterRange').data('daterangepicker').setStartDate(startDate);
-        //     $('#insightsFilterRange').data('daterangepicker').setEndDate(endDate);
-
-        //     this.insights.filterParameters.start = startDate;
-        //     this.insights.filterParameters.end = endDate;
-        // },
-        fetch: function() {
-            // this.insights.updateReport();
-        },
-    });
-
-    var router = new Router();
-    Backbone.history.start({ push_state: true });
 })(jQuery);
