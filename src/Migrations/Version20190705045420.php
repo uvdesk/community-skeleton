12bc647a709252b0e6c535e15b61f1142e3f72b4
piyushwebkul <50818826+piyushwebkul@users.noreply.github.com>
2019-07-04 19:08:36 +0530

    doctrine bundle specific config added

---
+++ src/Migrations/Version20190705045420.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190705045420 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
