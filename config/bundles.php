dbf5696b68a01eea0b298dab6ddaac0d9dfd1c9c
Akshay Kumar <akshay.kumar758@webkul.com>
2023-06-12 20:00:55 +0530

    Add base package configuration files

---
+++ config/bundles.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    Webkul\UVDesk\CoreFrameworkBundle\UVDeskCoreFrameworkBundle::class => ['all' => true],
+    Webkul\UVDesk\AutomationBundle\UVDeskAutomationBundle::class => ['all' => true],
+    Webkul\UVDesk\ExtensionFrameworkBundle\UVDeskExtensionFrameworkBundle::class => ['all' => true],
+    Webkul\UVDesk\MailboxBundle\UVDeskMailboxBundle::class => ['all' => true],
+    Webkul\UVDesk\SupportCenterBundle\UVDeskSupportCenterBundle::class => ['all' => true],
+    Webkul\UVDesk\ApiBundle\UVDeskApiBundle::class => ['all' => true],
+];
